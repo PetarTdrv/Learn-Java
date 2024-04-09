c4aa1f8f43758750ccda0a2ab452fda7418621e9
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-04-09 21:50:35 +0300

    Create speed_info.java

---
+++ Programming Basics/conditional_statements /lecture /speed_info.java
@@ -0,0 +1,22 @@
+package if_else_statements_lecture;
+import java.util.Scanner;
+
+public class speed_info {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double speed = Double.parseDouble(scanner.nextLine());
+
+        if (speed <= 10) {
+            System.out.println("slow");
+        } else if (speed <= 50) {
+            System.out.println("average");
+        } else if (speed <= 150) {
+            System.out.println("fast");
+        } else if (speed <= 1000) {
+            System.out.println("ultra fast");
+        } else {
+            System.out.println("extremely fast");
+        }
+    }
+}
