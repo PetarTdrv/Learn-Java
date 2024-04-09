10ec1aea23381b7a7e174c98d5a552dbb0ed6567
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-04-09 21:48:24 +0300

    Create excellent_result.java

---
+++ Programming Basics/conditional_statements /lecture /excellent_result.java
@@ -0,0 +1,14 @@
+package if_else_statements_lecture;
+import java.util.Scanner;
+
+public class excellent_grade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int grade = Integer.parseInt(scanner.nextLine());
+
+        if (grade >= 5) {
+            System.out.println("Excellent!");
+        }
+    }
+}
